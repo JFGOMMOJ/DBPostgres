fe8e58ce8044740ecb2140916a2f25b55b9fd562
JFGOMMOJ <jhonfredygomez@ucundinamarca.edu.co>
2025-04-02 23:58:07 -0500

    Segundo Commit

---
+++ src/main/java/com/example/DBPostgres/Model/Estudiante.java
@@ -0,0 +1,59 @@
+package com.example.DBPostgres.Model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+    private String nombre;
+    private String telefono;
+
+    @ManyToOne
+    @JoinColumn(name="profesor_id", nullable = false)
+    private Profesor profesor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "estudiante_materia", // Nombre de la tabla intermedia
+            joinColumns = @JoinColumn(name = "estudiante_id"),
+            inverseJoinColumns = @JoinColumn(name = "materia_id")
+    )
+    private List<Materia> materias;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(long codigo, String nombre, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/test/java/com/example/DBPostgres/DbPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBPostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
